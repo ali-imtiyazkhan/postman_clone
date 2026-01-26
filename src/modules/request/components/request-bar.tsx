bb69768ab01c0b032be35a5f6c5cd307e41c6c80
ali-imtiyazkhan <itzkhantijara@gmail.com>
2026-01-26 12:45:33 +0530

    request-bar minichanged

--- src/modules/request/components/request-bar.tsx
+++ src/modules/request/components/request-bar.tsx
@@ -6,7 +6,6 @@ import {
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
@@ -17,13 +16,13 @@ import { useRunRequest } from '../hooks/request'
 import { toast } from 'sonner'
 
 interface Props {
-    tab: RequestTab,
-    updateTab: (id: string, data: Partial<RequestTab>) => void;
+  tab: RequestTab,
+  updateTab: (id: string, data: Partial<RequestTab>) => void;
 }
 
 const RequestBar = ({ tab, updateTab }: Props) => {
 
-  const {mutateAsync , isPending , isError} = useRunRequest(tab?.requestId!);
+  const { mutateAsync, isPending } = useRunRequest(tab?.requestId!);
   const requestColorMap: Record<string, string> = {
     GET: "text-green-500",
     POST: "text-blue-500",
@@ -34,7 +33,7 @@ const RequestBar = ({ tab, updateTab }: Props) => {
   const onSendRequest = async () => {
     try {
       const res = await mutateAsync();
-      
+
       toast.success('Request sent successfully!');
     } catch (error) {
       toast.error('Failed to send request.');
@@ -44,8 +43,8 @@ const RequestBar = ({ tab, updateTab }: Props) => {
   return (
     <div className='flex flex-row items-center justify-between bg-zinc-900 rounded-md px-2 py-2 w-full'>
       <div className="flex flex-row items-center gap-2 flex-1">
-        <Select 
-          value={tab.method} 
+        <Select
+          value={tab.method}
           onValueChange={(value) => updateTab(tab.id, { method: value })}
         >
           <SelectTrigger className={`w-24 ${requestColorMap[tab.method] || "text-gray-500"}`}>
@@ -60,17 +59,17 @@ const RequestBar = ({ tab, updateTab }: Props) => {
             </SelectGroup>
           </SelectContent>
         </Select>
-        
-        <Input 
-          value={tab.url || ''} 
+
+        <Input
+          value={tab.url || ''}
           onChange={(e) => updateTab(tab.id, { url: e.target.value })}
           placeholder="Enter URL"
           className="flex-1"
         />
       </div>
-      
-      <Button 
-      type='submit'
+
+      <Button
+        type='submit'
         onClick={onSendRequest}
         disabled={isPending || !tab.url}
         className="ml-2 text-white  font-bold bg-indigo-500 hover:bg-indigo-600"
